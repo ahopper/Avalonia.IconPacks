c08a9acde89d2d38989837784ab55d342b747550
ahopper <alan@samsararesearch.com>
2018-10-18 11:14:20 +0100

    Add MahApps icon exporter project

---
+++ MetroIconPackExport/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MahApps.Metro.IconPacks;
+using System;
+using System.IO;
+using System.Windows;
+
+namespace MetroIconPackExport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            exportIcons();
+        }
+        void exportIcons()
+        {
+            exportIconPack("Material", new PackIconMaterial());
+            exportIconPack("MaterialLight", new PackIconMaterialLight());
+            exportIconPack("FontAwesome", new PackIconFontAwesome());
+            exportIconPack("Octicons", new PackIconOcticons());
+            exportIconPack("Modern", new PackIconModern());
+            exportIconPack("Entypo+", new PackIconEntypo());
+            exportIconPack("SimpleIcons", new PackIconSimpleIcons());
+            exportIconPack("WeatherIcons", new PackIconWeatherIcons());
+            exportIconPack("Typicons", new PackIconTypicons());
+        }
+  
+        void exportIconPack<P>(string title, PackIconControl<P> pack) where P:Enum
+        {
+            //TODO use proper xml writer
+            using (var file = File.CreateText($"..\\..\\..\\Icons\\{title}.xaml"))
+            {
+                file.WriteLine("<Styles xmlns=\"https://github.com/avaloniaui\"");
+                file.WriteLine("    xmlns:x = \"http://schemas.microsoft.com/winfx/2006/xaml\" >");
+                file.WriteLine("    <Style>");
+                file.WriteLine("        <Style.Resources>");
+
+                var icons = Enum.GetValues(typeof(P));
+
+                foreach (var icon in icons)
+                {
+                    var name = icon.ToString();
+                    pack.Kind = (P)icon;
+                    var data = pack.Data;
+
+                    file.WriteLine($"            <GeometryDrawing x:Key=\"{title}.{name}\" Brush=\"#FF000000\" Geometry=\"{data}\"/>");
+                }
+                file.WriteLine("        </Style.Resources>");
+                file.WriteLine("    </Style>");
+                file.WriteLine("</Styles>");
+            }
+        }
+    }
+}
